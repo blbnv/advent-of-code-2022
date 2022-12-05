d3c15ea21ae69cbc6b33ef08ca67ee54d4c27f11
Oleksandr Balabanov <bbalabanovv@gmail.com>
2022-12-05 16:38:45 +0100

    day 3 part 2

--- 3/main.swift
+++ 3/main.swift
@@ -9,6 +9,10 @@ print("Part 1")
 let prioritiesSum = lines.reduce(0) { $0 + bagPriority($1) }
 print("Priorities sum: \(prioritiesSum)")
 
+print("Part 2")
+let badgePrioritiesSum = badgePrioritySum(lines)
+print("Badge priorities sum: \(badgePrioritiesSum)")
+
 func bagPriority(_ line: String) -> Int {
     let lineArray = Array(line)
     let halfOne = Set(lineArray[0...line.count/2 - 1])
@@ -17,4 +21,30 @@ func bagPriority(_ line: String) -> Int {
     let asciiValue = Int(priority.asciiValue!)
     let score = priority.isLowercase ? asciiValue - 96 : asciiValue - 38
     return score
+}
+
+func badgePrioritySum(_ lines: [String]) -> Int {
+    let divided = lines.chunked(into: 3)
+    return divided.reduce(0) { $0 + singleChunkPriority($1) }
+}
+
+func singleChunkPriority(_ chunk: [String]) -> Int {
+    var set1 = Set<Character>(), set2 = Set<Character>()
+    chunk[0].forEach { set1.insert($0) }
+    chunk[1].forEach { set2.insert($0) }
+    let set12 = set1.intersection(set2)
+    var set3 = Set<Character>()
+    chunk[2].forEach { set3.insert($0) }
+    let element = set12.intersection(set3).first!
+    let asciiValue = Int(element.asciiValue!)
+    let score = element.isLowercase ? asciiValue - 96 : asciiValue - 38
+    return score
+}
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
 }
